0d84e962db4f48deacb4b17d8bf2a2d43adfaee2
mshafaetjamil <155761781+mshafaetjamil@users.noreply.github.com>
2024-02-15 21:16:44 +0600

    first solve

---
+++ Assignment1/71A_Way Too Long Words/Program.cs
@@ -0,0 +1,21 @@
+﻿{
+    int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i < n; i++)
+    {
+        string word = Console.ReadLine();
+        if (word.Length > 10)
+        {
+            string abbreviatedWord = Abbreviate(word);
+            Console.WriteLine(abbreviatedWord);
+        }
+        else
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
+
+static string Abbreviate(string word)
+{
+    return $"{word[0]}{word.Length - 2}{word[^1]}";
+}
