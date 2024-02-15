ddd60a1fcd5a603bc31c7d0c23d8916dccf178af
mshafaetjamil <155761781+mshafaetjamil@users.noreply.github.com>
2024-02-15 23:11:45 +0600

    3rd

---
+++ Assignment1/231A-Team/Program.cs
@@ -0,0 +1,31 @@
+﻿{
+    // Read the number of problems
+    int n = int.Parse(Console.ReadLine());
+
+  
+    int problemsToImplement = 0;
+
+ 
+    for (int i = 0; i < n; i++)
+    {
+ 
+        string[] opinions = Console.ReadLine().Split();
+
+ 
+        int petya = int.Parse(opinions[0]);
+        int vasya = int.Parse(opinions[1]);
+        int tonya = int.Parse(opinions[2]);
+
+ 
+        int sureCount = petya + vasya + tonya;
+
+        if (sureCount >= 2)
+        {
+            problemsToImplement++;
+        }
+    }
+
+  
+    Console.WriteLine(problemsToImplement);
+}
+
--- Assignment1/4A-Watermelon/obj/Debug/net8.0/4A-Watermelon.AssemblyInfo.cs
+++ Assignment1/4A-Watermelon/obj/Debug/net8.0/4A-Watermelon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4A-Watermelon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f5472b386b46a5f2349c01c509d26cbdcd08b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab908a2e6d3171491bbdf451d339aa4b9b439a0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("4A-Watermelon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4A-Watermelon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
