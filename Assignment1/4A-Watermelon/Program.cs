66f5472b386b46a5f2349c01c509d26cbdcd08b8
mshafaetjamil <155761781+mshafaetjamil@users.noreply.github.com>
2024-02-15 21:44:46 +0600

    second solve

---
+++ Assignment1/4A-Watermelon/Program.cs
@@ -0,0 +1,15 @@
+﻿{
+    int w = int.Parse(Console.ReadLine());
+
+    string result = CanDivideWatermelon(w);
+
+    Console.WriteLine(result);
+}
+
+static string CanDivideWatermelon(int w)
+{
+    if (w < 2 || w % 2 != 0)
+        return "NO";
+    else return "YES";
+
+}
