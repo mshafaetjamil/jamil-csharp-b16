ca4f2120bbf836c268553810066eefd6385854da
mshafaetjamil <155761781+mshafaetjamil@users.noreply.github.com>
2024-02-23 12:11:19 +0600

    assignment 2

---
+++ Assignment2/Task1/SpaceShip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public abstract class SpaceShip
+    {
+        public double Speed { get; set; }
+        public abstract object CloneShip();
+
+        public SpaceShip(double speed)
+        {
+            Speed = speed;
+        }
+    }
+}
