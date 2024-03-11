25fac347b6cf1fb31b32de691a5c0aca4f65382d
mshafaetjamil <155761781+mshafaetjamil@users.noreply.github.com>
2024-03-11 19:58:58 +0600

    task1

---
+++ Exam2/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+{ 
+    Random random = new Random(DateTime.Now.Millisecond);
+
+
+    long fileSizeGB = 5;
+    long fileSizeBytes = fileSizeGB * 1024*1024*1024;
+
+    string folderPath = @"H:\DevSCS\jamil-csharp-b16\Exam2\";
+    Directory.CreateDirectory(folderPath);
+
+    string filePath = Path.Combine(folderPath, "randomfile.txt");
+
+    using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+    {
+
+
+        for (int i = 0; i < fileSizeBytes; i++)
+        {
+            char randomChar = GetRandomChar(random);
+            byte[] charBytes = BitConverter.GetBytes(randomChar);
+            fs.Write(charBytes, 0, charBytes.Length);
+        }
+    }
+    Console.WriteLine("File Created:"+ filePath);
+
+}
+    static char GetRandomChar(Random random)
+    {
+        return (char)random.Next('A', 'Z' +1);
+    }
+
+
---
+++ Exam2/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+string OrgFilePath = "randomfile.txt";
+string NewFolder = "Folder";
+Directory.CreateDirectory(NewFolder);
+
+byte[] originalFileBytes = File.ReadAllBytes(OrgFilePath);
+
+long chunkSizeBytes = 100 * 1024;
+int numChunks = (int)Math.Ceiling((int)Math.Ceiling((double)originalFileBytes.Length / chunkSizeBytes);
